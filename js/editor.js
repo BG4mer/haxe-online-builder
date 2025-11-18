69f34d22a4b70aa568abb5913d97a3f3802af449
BG4mer <bostonking3235@gmail.com>
2025-11-17 21:30:46 -0800

    Create editor.js

---
+++ js/editor.js
@@ -0,0 +1,47 @@
+let editor;
+let files = {
+    "Main.hx": "class Main {\n\tstatic function main() {\n\t\ttrace('Hello from mobile compiler!');\n\t}\n}"
+};
+
+let currentFile = "Main.hx";
+
+// Monaco loader
+require.config({ paths: { vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.44.0/min/vs" } });
+
+require(["vs/editor/editor.main"], function () {
+
+    editor = monaco.editor.create(document.getElementById("editor"), {
+        value: files[currentFile],
+        language: "haxe",
+        theme: "vs-dark",
+        automaticLayout: true
+    });
+
+    loadFileList();
+});
+
+function loadFileList() {
+    const panel = document.getElementById("fileList");
+    panel.innerHTML = "";
+
+    Object.keys(files).forEach(name => {
+        const div = document.createElement("div");
+        div.innerText = name;
+        div.onclick = () => switchFile(name);
+        panel.appendChild(div);
+    });
+}
+
+function switchFile(name) {
+    files[currentFile] = editor.getValue();
+    currentFile = name;
+    editor.setValue(files[name]);
+}
+
+document.getElementById("addFileBtn").onclick = () => {
+    const newName = prompt("New file name:");
+    if (!newName) return;
+    files[newName] = "";
+    loadFileList();
+    switchFile(newName);
+};
